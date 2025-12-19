acc911cba588433a352de2e244260665780b73ab
Ravishka Praveen <dmrpbandara@students.nsbm.ac.lk>
2025-12-19 17:52:16 +0530

    Added the initial intro page of the app

---
+++ lib/pages/introPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Intropage extends StatelessWidget {
+  const Intropage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    );
+  }
+}
