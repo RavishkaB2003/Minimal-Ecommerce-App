296ef89df43bbd5ed3c6ef18c6b4ae39d520ee5b
Ravishka Praveen <dmrpbandara@students.nsbm.ac.lk>
2025-12-27 09:56:24 +0530

    configured the adapter and opened a hive box in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:minimal_ecommerce/models/products.dart';
 import 'package:minimal_ecommerce/models/shop.dart';
 import 'package:minimal_ecommerce/pages/cart_page.dart';
 import 'package:minimal_ecommerce/pages/intro_page.dart';
@@ -8,15 +9,15 @@ import 'package:minimal_ecommerce/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-
-
   //stat hive
   await Hive.initFlutter();
 
-  //open hive box
-  await Hive.openBox('shopBox');
+  //Registering the adapter
+  Hive.registerAdapter(ProductsAdapter());
 
-  
+  //opening a box (this is like a database table) to store products
+  await Hive.openBox<List>('cart_box');
+
   runApp(
     MultiProvider(
       providers: [
